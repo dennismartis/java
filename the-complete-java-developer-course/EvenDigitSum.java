08c1a86d564db4f9352101792e970f755e1aa1fa
Dennis Martis <martisdennis@gmail.com>
2018-08-15 18:49:45 +0200

    Added EvenDigitSum

---
+++ the-complete-java-developer-course/EvenDigitSum.java
@@ -0,0 +1,20 @@
+public class EvenDigitSum {
+    public static void main(String[] args) {
+        System.out.println(getEvenDigitSum(2747592));
+    }
+    public static int getEvenDigitSum(int number) {
+        if (number < 0 ) {
+            return -1;
+        }
+        int sum = 0;
+        while (number > 0) {
+            int digit = number % 10;
+            if (digit % 2 == 0) {
+                sum += digit;
+            }
+            
+            number /= 10;
+        }
+        return sum;
+    }
+}
